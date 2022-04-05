7a44b660746176fb43ec3b912ddd45ba4bf24a38
bsal cgdl <bishalsigdel885@gmail.com>
2022-04-05 20:29:17 +0545

    lab exercise three completed with all the task coding challenges and homework

---
+++ LAB2/helloconstraints/app/src/main/java/com/example/helloconstraints/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.helloconstraints;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+
+
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+
+        }
+        if(mCount%2==0){
+            view.setBackgroundColor(Color.GREEN);
+        }
+        else if(mCount%2!=0){
+            view.setBackgroundColor(Color.YELLOW);
+        }
+        view = findViewById(R.id.zero);
+        view.setBackgroundColor(Color.MAGENTA);
+    }
+
+    public void countZero(View view) {
+        mCount = 0;
+        mShowCount.setText(Integer.toString(mCount));
+        view.setBackgroundColor(Color.DKGRAY);
+        view = findViewById(R.id.button_count);
+        view.setBackgroundColor(Color.GREEN);
+    }
+}
